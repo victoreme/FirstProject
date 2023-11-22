71ade02af21a8da14ae0d3b572181a048047e8c0
Support <Support@WEMA-TRAINING-0>
2023-11-22 07:53:48 +0100

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FirstProject;
+
+public class User
+{
+    public static void Main(string[] args)
+    {
+        double based, height, area;
+ 
+        Console.WriteLine("Enter the base:");
+        based = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the height:");
+        height = Convert.ToDouble(Console.ReadLine());
+
+        area = (based * height) / 2;
+
+        Console.WriteLine("The area is " + area);
+    }
+}
---
+++ FirstProject/User.cs
@@ -0,0 +1,36 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+   *//* public class User
+    {
+*//*         public static void main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World");
+
+        }
+
+*//*
+        public class Program
+        {
+           public static void Main(string[] args)
+            {
+                double based, height, area;
+
+                Console.WriteLine("Enter the base:");
+                based = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the height:");
+                height = Convert.ToDouble(Console.ReadLine());
+
+                area = (based * height) / 2;
+
+                Console.WriteLine("The area is " + area);
+            }
+        }
+    }*/
